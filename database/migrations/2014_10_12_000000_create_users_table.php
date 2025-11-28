6ac66c3f2af174a62d7df2391985e97afad21bb4
joseluchosac <joseluchosac@gmail.com>
2025-11-28 11:49:33 -0500

    Primer commit

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('username', 15)->unique();
+            $table->string('nombres', 50)->nullable();
+            $table->string('apellidos', 50)->nullable();
+            $table->string('email')->unique()->nullable();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+            $table->bigInteger('rol_id')->nullable();
+            $table->tinyInteger('status')->default(1);
+            $table->string('code_restore', 6)->nullable();
+            $table->dateTime('last_login_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
